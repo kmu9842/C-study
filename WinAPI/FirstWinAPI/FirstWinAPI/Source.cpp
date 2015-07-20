e874800a48135d72534eb19ae21338258a230fb0
min <kmu9842@naver.com>
2015-07-20 20:57:08 +0900

    .
    .

--- WinAPI/FirstWinAPI/FirstWinAPI/Source.cpp
+++ WinAPI/FirstWinAPI/FirstWinAPI/Source.cpp
@@ -1,9 +1,21 @@
 #include <windows.h>
 
-LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
-HINSTANCE g_hInst;
-LPCTSTR lpszClass = TEXT("First");
+LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM); // 사용자와 시스템이 보내오는 메시지를 처리하는 역할
+// WndProc와 WinMain둘다 필수 함수
+// WinMain은 윈도우를 만들고 표시하기만 할뿐 대부분의 일은 WndProc에서 이루어짐
 
+HINSTANCE g_hInst; 
+LPCTSTR lpszClass = TEXT("First");	// 윈도우 클래스를 정의하는데 사용됨
+
+// APIENTRY는 윈도우즈의 표준 호출 규약 _stdcall을 사용한다는 뜻.
+// WinMain의 매개 변수 
+// hInstance 프로그램의 인스턴스 핸들
+// hPrevInstance 바로 앞에 실행된 현재 프로그램의 인스턴스 핸들이다. 없을 경우 NULL이 되면 Win32에서는 항상 NULL이다. 16비트를 지원하기 위한 인수
+// lpszCmdParam 명령행으로 입력된 프로그램 인수. 도스의 argv인수에 해당하며 실행 직후 열파일의 경로가 전달된다.
+// nCmdShow 프로그램이 실행될 형태이며 최소화, 보통 모양 등이 전달된다.
+
+// 인스턴스라는 말은 실행중인 프로그램 하나를 칭하는 용어이다. 
+// 프로그램 내부에서 자신을 가리키는 1인칭 대명사 같은것
 int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpszCmdParam, int nCmdShow) {
 
 	HWND hWnd; // 윈도우 핸들
@@ -16,7 +28,7 @@ int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpszCmd
 	WndClass.hbrBackground = (HBRUSH)GetStockObject(WHITE_BRUSH);
 	WndClass.hCursor = LoadCursor(NULL, IDC_ARROW);
 	WndClass.hIcon = LoadIcon(NULL, IDI_APPLICATION);
-	WndClass.hInstance = hInstance;
+	WndClass.hInstance = hInstance; 
 	WndClass.lpfnWndProc = WndProc;
 	WndClass.lpszClassName = lpszClass;
 	WndClass.lpszMenuName = NULL;
